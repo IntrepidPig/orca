86b0dc915290ef55d45d8276b6d14474fe0e2365
Benny A <virusdownloader7@gmail.com>
2017-08-16 19:52:52 -0700

    Added basic blocking ratelimiting (2 seconds)

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -2,6 +2,9 @@
 pub mod auth;
 
 use std::io::Read;
+use std::time::{Duration, Instant};
+use std::thread;
+
 use json;
 use http::{Client, Request, Method, Url};
 use http::header::{UserAgent, Authorization, Bearer};
@@ -12,15 +15,21 @@ pub struct Connection {
 	pub auth: Option<auth::Auth>,
 	pub useragent: UserAgent,
 	pub client: Client,
+	lastreq: Instant,
 }
 
 impl Connection {
 	pub fn new(appname: String, appversion: String, appauthor: String) -> Connection {
 		let useragent = UserAgent::new(format!("orca:{}:{} (by {})", appname, appversion, appauthor));
-		Connection { auth: None, useragent, client: Client::new().unwrap() }
+		Connection { auth: None, useragent, client: Client::new().unwrap(), lastreq: Instant::now() }
 	}
 	
 	pub fn run_request(&self, req: Request) -> Result<json::Value, ()> {
+		if self.lastreq.elapsed() < Duration::new(2, 0) {
+			println!("Ratelimiting");
+			thread::sleep(Instant::now() - self.lastreq);
+		}
+		
 		if let Ok(mut response) = self.client.execute(req) {
 			let mut out = String::new();
 			match response.read_to_string(&mut out) {
