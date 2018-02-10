85b1490dff81f66f87f8c152bc59413c11d384c4
Benny A <virusdownloader7@gmail.com>
2018-02-09 22:11:26 -0800

    Reflect changes in App struct

--- src/lib.rs
+++ src/lib.rs
@@ -78,6 +78,7 @@ extern crate tokio_core;
 extern crate url;
 
 use std::collections::{HashMap, VecDeque};
+use std::sync::Arc;
 
 use json::Value;
 use hyper::{Method, Request};
@@ -94,7 +95,7 @@ pub mod data;
 /// Errors
 pub mod errors;
 
-pub use net::auth::{self, InstalledAppError, OAuthApp, ResponseGenFn};
+pub use net::auth::{self, InstalledAppError, OAuthApp, ResponseGenFn, Scopes};
 pub use net::{Connection, LimitMethod};
 pub use errors::RedditError;
 
@@ -125,17 +126,35 @@ impl App {
 			conn: Connection::new(appname, appversion, appauthor)?,
 		})
 	}
-
-	/// Return an Auth object for use with API calls that require a user account to work
+	
+	/// Authorize this app as a script
 	/// # Arguments
-	/// * `username` - Username of the user to be authorized as
-	/// * `password` - Password of the user to be authorized as
-	/// * `oauth` - Oauth app type
-	/// # Returns
-	/// A result containing either an Auth object or a certain error
-	/// To use place it in the auth field of a connection struct
-	pub fn authorize(&mut self, oauth: &net::auth::OAuthApp) -> Result<(), Error> {
-		self.conn.auth = Some(OAuth::new(&self.conn, oauth)?);
+	/// * `id` - The app id registered on Reddit
+	/// * `secret` - The app secret registered on Reddit
+	/// * `username` - The username of the user to authorize as
+	/// * `password` - The password of the user to authorize as
+	pub fn authorize_script(&mut self, id: &str, secret: &str, username: &str, password: &str) -> Result<(), Error> {
+		let auth = OAuth::create_script(&self.conn, id, secret, username, password)?;
+		self.conn.auth = Some(auth);
+		Ok(())
+	}
+	
+	/// Authorize this app as an installed app
+	/// # Arguments
+	/// * `conn` - A reference to the connection to authorize
+	/// * `id` - The app id registered on Reddit
+	/// * `redirect` - The app redirect URI registered on Reddit
+	/// * `response_gen` - An optional function that generates a hyper Response to give to the user
+	/// based on the result of the authorization attempt. The signature is `(Result<String, InstalledAppError) -> Result<Response, Response>`.
+	/// The result passed in is either Ok with the code recieved, or Err with the error that occurred.
+	/// The value returned should usually be an Ok(Response), but you can return Err(Response) to indicate
+	/// that an error occurred within the function.
+	/// * `scopes` - A reference to a Scopes instance representing the capabilites you are requesting
+	/// as an installed app.
+	pub fn authorize_installed_app<I: Into<Option<Arc<ResponseGenFn>>>>
+	(&mut self, id: &str, redirect: &str, response_gen: I, scopes: &Scopes) -> Result<(), Error> {
+		let auth = OAuth::create_installed_app(&self.conn, id, redirect, response_gen, scopes)?;
+		self.conn.auth = Some(auth);
 		Ok(())
 	}
 
